e260825dbdc7574734da4d9856d0569ca6dd2a30
itsmems <manishshrestha0803@gmail.com>
2022-07-21 19:50:00 +0545

    userlogin css style update

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ document.querySelector('#search-btn').onclick =()=>{
     searchForm.classList.toggle('active');
 }
 
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+
 
 window.onscroll = () =>{
     

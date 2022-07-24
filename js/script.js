ffac28752307d5b209e573808a5efb9bab07325c
itsmems <manishshrestha0803@gmail.com>
2022-07-24 16:49:03 +0545

    home page update

--- js/script.js
+++ js/script.js
@@ -97,3 +97,24 @@ function setSuccessFor(input) {
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
+// slider for header section
+var swiper = new Swiper(".books-slider", {
+	loop:true,
+	centeredSlides: true,
+	autoplay: {
+	  delay: 9500,
+	  disableOnInteraction: false,
+	},
+	breakpoints: {
+	  0: {
+		slidesPerView: 1,
+	  },
+	  768: {
+		slidesPerView: 2,
+	  },
+	  1024: {
+		slidesPerView: 3,
+	  },
+	},
+  });
